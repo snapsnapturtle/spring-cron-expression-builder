5737a6e18706d420cd298f29249a12524b73cfac
Jonah Möller <jonah.moeller@googlemail.com>
2024-07-19 21:33:51 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,32 @@
 module.exports = {
-    root: true,
-    env: {browser: true, es2020: true},
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended-type-checked',
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json", "./tsconfig.app.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: __dirname,
-    },
-    plugins: ['react-refresh'],
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        'react-refresh/only-export-components': [
-            'warn',
-            {allowConstantExport: true},
-        ],
-    },
-}
+  },
+};
